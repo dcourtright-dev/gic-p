e37b55b2e0ea3a746755dd0655cbb8f0517cbc29
Daniel Courtright <daniel.courtright1@gmail.com>
2026-02-14 19:24:37 -0800

    Refactor main.c for improved readability and structure

--- main.c
+++ main.c
@@ -1,129 +1,140 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <ctype.h>
-#include <errno.h>
 #include <stdbool.h>
 
-// Return Codes
+// Return codes
 #define SUCCESS 0
-#define ERR_NO_GIT 1
-#define ERR_NO_FILES 2
-#define ERR_COMM_REJ 3
-#define ERR_CMD_FAIL 4
+#define ERR_CMD_FAIL 1
 
-void help() {
-	printf("Usage: gic [options]\n");
-	printf("Options:\n");
-	printf("   -f    Force Push\n");
-	printf("   -v    Verbose Output\n");
-	printf("   -h    Show this help\n");
+void print_help(void) {
+    printf("Usage: gic [options]\n");
+    printf("Options:\n");
+    printf("  -f    Force push\n");
+    printf("  -v    Verbose output\n");
+    printf("  -h    Show this help\n");
 }
 
-// Runs command
-void run(char *cmd, bool show) {
-	if (show) {
-		printf("$ %s\n", cmd);
-	}
+// Run a shell command
+int run_command(const char *cmd, bool verbose) {
+    if (verbose) {
+        printf("$ %s\n", cmd);
+    }
 
-	int result = system(cmd);
-	if (result != 0) {
-		fprintf(stderr, "Command Failed: %s\n", cmd);
-	}
+    int result = system(cmd);
+    if (result != 0) {
+        fprintf(stderr, "Command failed: %s\n", cmd);
+        return ERR_CMD_FAIL;
+    }
+    return SUCCESS;
 }
 
-// Check status of git
-int status() {
-	FILE *fp;
-	char output[1024];
-	
-	// Run git status and read output
-	fp = popen("git status --short", "r");
-	if (fp == NULL) {
-		perror("Failed to run git");
-		return 1;
-	}
+// Show git status
+int show_status(void) {
+    FILE *fp = popen("git status --short", "r");
+    if (fp == NULL) {
+        perror("Failed to run git");
+        return ERR_CMD_FAIL;
+    }
 
-	// Print stdout of git status
-	while (fgets(output, sizeof(output), fp) != NULL) {
-		printf("%s", output);
-	}
-	pclose(fp);
-	return 0;
+    char output[1024];
+    while (fgets(output, sizeof(output), fp) != NULL) {
+        printf("%s", output);
+    }
+    
+    pclose(fp);
+    return SUCCESS;
 }
 
-// Stages files before commit
-int stage(char *filename) {
-	char cmd[512];
-	snprintf(cmd, sizeof(cmd),  "git add %s", filename);
-
-	run(cmd, true);
+// Stage a file
+int stage_file(const char *filename, bool verbose) {
+    char cmd[512];
+    snprintf(cmd, sizeof(cmd), "git add %s", filename);
+    return run_command(cmd, verbose);
 }
 
-// Commit
-int commit(char *msg) {
-	char cmd[512];
-	snprintf(cmd, sizeof(cmd), "git commit -m \"%s\"", msg);
-	
-	run(cmd, true);
-	return 0;
+// Commit with message
+int commit_changes(const char *msg, bool verbose) {
+    char cmd[512];
+    snprintf(cmd, sizeof(cmd), "git commit -m \"%s\"", msg);
+    return run_command(cmd, verbose);
 }
 
 // Push changes
-int push(bool force, bool verbose) {
-	char cmd[256];
-
-	if (force) {
-		snprintf(cmd, sizeof(cmd), "git push --force");
-	} else {
-		snprintf(cmd, sizeof(cmd), "git push");
-	}
-
-	if (verbose) {
-		printf("$ %s\n", cmd);
-	}
-
-	return system(cmd) == 0;
+int push_changes(bool force, bool verbose) {
+    char cmd[256];
+    
+    if (force) {
+        snprintf(cmd, sizeof(cmd), "git push --force");
+    } else {
+        snprintf(cmd, sizeof(cmd), "git push");
+    }
+    
+    return run_command(cmd, verbose);
 }
 
-// Main Script
+// Remove trailing newline from fgets input
+void strip_newline(char *str) {
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+    }
+}
+
 int main(int argc, char *argv[]) {
-	bool force_push = false;
-	bool verbose = false;
-	char buff[100];
-	int n = 50;
+    bool force_push = false;
+    bool verbose = false;
+    char buffer[256];
 
+    // Parse command line arguments
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-f") == 0) {
+            force_push = true;
+        } else if (strcmp(argv[i], "-v") == 0) {
+            verbose = true;
+        } else if (strcmp(argv[i], "-h") == 0) {
+            print_help();
+            return SUCCESS;
+        } else {
+            fprintf(stderr, "Unknown option: %s\n", argv[i]);
+            print_help();
+            return ERR_CMD_FAIL;
+        }
+    }
 
-	for (int i=1; i < argc; i++) {
-		if (strcmp(argv[i], "-f") == 0) {
-			force_push = true;
-		} else if (strcmp(argv[i], "-v" == 0)) {
-			verbose = true;
-		} else if (strcmp(argv[i], "-h" == 0)) {
-			help();
-			return 0;
-		}
-	}
-	
-	status();
-	
-	// Stage Files
-	printf("File to stage? : ");
-	fgets(buff, n, stdin);
-	printf("File to stage: %s", buff);
-	stage(buff);
-	
-	// Commit Files
-	printf("Commit Message? : ");
-	fgets(buff, n, stdin);
-	printf("Commit Message: %s", buff);
-	commit(buff);
+    // Show current status
+    show_status();
 
-	// Push
-	printf("Pushing Files...");
-	push(force_push, verbose);
+    // Stage files
+    printf("File to stage: ");
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+        fprintf(stderr, "Failed to read input\n");
+        return ERR_CMD_FAIL;
+    }
+    strip_newline(buffer);
+    
+    if (stage_file(buffer, verbose) != SUCCESS) {
+        return ERR_CMD_FAIL;
+    }
 
-	return 0;
+    // Commit
+    printf("Commit message: ");
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+        fprintf(stderr, "Failed to read input\n");
+        return ERR_CMD_FAIL;
+    }
+    strip_newline(buffer);
+    
+    if (commit_changes(buffer, verbose) != SUCCESS) {
+        return ERR_CMD_FAIL;
+    }
+
+    // Push
+    printf("Pushing files...\n");
+    if (push_changes(force_push, verbose) != SUCCESS) {
+        return ERR_CMD_FAIL;
+    }
+
+    printf("Done!\n");
+    return SUCCESS;
 }
